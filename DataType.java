6d45a5127e671925a85fcf81289b714480152a70
Aditya Sonwane <aadus.info@gmail.com>
2022-09-20 22:36:13 +0530

    DateTypes in java

---
+++ DataType.java
@@ -0,0 +1,20 @@
+class DataType{
+    public static void main(String[] args) {
+        int a = 20; // intger 
+        float b = 3.14f;  // floating point
+        double d = 3453443d; // double 
+        boolean c = true;
+        String s = "aditya";
+        char e = 'c';
+        
+        System.out.println("integer "+a);
+        System.out.println("float "+b);
+        System.out.println("double "+d);
+        System.out.println("boolean "+c);
+        System.out.println("String "+s);
+        System.out.println("Char "+e);
+        
+    
+    }
+
+}
