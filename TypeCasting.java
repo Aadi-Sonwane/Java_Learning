1601124f9334fa809b55f799ab22644aa6344fb5
Aditya Sonwane <aadus.info@gmail.com>
2022-09-20 22:46:38 +0530

    Type Casting in java

---
+++ TypeCasting.java
@@ -0,0 +1,18 @@
+class TypeCasting{
+    public static void main(String[] args){
+        System.out.println("we just tring to convert integer to float");
+        int a = 10;
+        int b = 17;
+        float conv =( float) a + b;
+        System.out.println("The type casting is " + conv);
+        
+        // or 
+        
+        int intvariable = 4;
+        float floating = (float) intvariable;
+        System.out.println("The type casting using varibale is :" + floating);
+        
+        
+        
+    }
+}
