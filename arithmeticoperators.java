bc549c377e6bed8cc43e84c4a48e5917a4657147
Aditya Sonwane <aadus.info@gmail.com>
2022-09-20 23:04:33 +0530

    Arithmetic Operators

---
+++ arithmeticoperators.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class arithmeticoperators
+{
+    public static void main(String[] args){
+        float a = 10;
+        float b = 30;
+        int c = 5;
+        
+        // Arthmatic operators 
+         
+         float add = (a + b);
+         float sub = a - b;
+         float mul = a * b;
+         float div = a / b;
+         float mod = a % b;
+         int inc = ++c;
+         int dic = --c;
+         int incp = c++;
+         int dicp = c--;
+         
+         System.out.println("Athematic oprations " +add);
+         
+         System.out.println("Athematic oprations " +sub);
+         System.out.println("Athematic oprations " +mul);
+         System.out.println("Athematic oprations " +div);
+         System.out.println("Athematic oprations " +mod);
+         System.out.println("Athematic oprations " +inc);
+         System.out.println("Athematic oprations " +incp);
+         System.out.println("Athematic oprations " +dic);
+         System.out.println("Athematic oprations " +dicp);
+          
+    }
+}
